8b38a10af5910a93d92bf176977c2ef8d3e3626c
vagrant <vagrant@vagrant.vm>
2017-02-04 22:04:51 +0000

    join contacts and experience collections to find all work experience for contact

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  var db = mongojs('web:wef34982sdf@localhost:27017/resume');
+  var contacts = db.collection('contacts');
+  
+  contacts.aggregate([
+  	{
+      $lookup:
+        {
+          from: "experience",
+          localField: "email",
+          foreignField: "email",
+          as: "work_history"
+        }
+   }
+  ], function(err, resume) {
+  	if (!err) {
+  	  res.json(resume);
+  	}
+  });
 });
 
 module.exports = router;
